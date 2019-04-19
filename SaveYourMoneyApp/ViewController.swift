20f14d5ba48c5a5f7621ba713fbbbdbf7dd6680c
Angel Chou <cat77120377@hotmail.com>
2019-04-19 15:07:56 +0800

    Initial Commit

---
+++ SaveYourMoneyApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SaveYourMoneyApp
+//
+//  Created by Shun-Ching, Chou on 2019/4/19.
+//  Copyright © 2019 Shun-Ching, Chou. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
